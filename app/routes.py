f31c8f618532bbef0c38125cad1d327cd06b03ce
Joong Sim <joonjooly@gmail.com>
2020-06-17 16:27:24 -0700

    added login functionality

--- app/routes.py
+++ app/routes.py
@@ -1,14 +1,15 @@
-from flask import render_template, flash, redirect, url_for
-from flask_login import current_user, login_user, logout_user
+from flask import render_template, flash, redirect, url_for, request
+from flask_login import current_user, login_user, logout_user, login_required
 from app import app
 from app.forms import LoginForm
 from app.models import User
+from werkzeug.urls import url_parse
 
 
 @app.route('/')
 @app.route('/index')
+@login_required
 def index():
-    user = {'username': 'Joong'}
     posts = [
         {
             'author': {'username': 'John'},
@@ -19,7 +20,7 @@ def index():
             'body': 'The Avengers movie was so cool!'
         }
     ]
-    return render_template('index.html', title='Home', user=user, posts=posts)
+    return render_template('index.html', title='Home', posts=posts)
 
 
 @app.route('/login', methods=['GET', 'POST'])
@@ -37,7 +38,19 @@ def login():
             flash('Invalid username or password')
             return redirect(url_for('login'))
         login_user(user, remember=form.remember_me.data)
-        return redirect(url_for('index'))
+        # next page redirect
+        # get next page url from user request
+        next_page = request.args.get('next')
+        # redirect to index if not set or set to full URL with domain name
+        # second case necessary to prevent attackers from redirecting to
+        # malicious site
+        # url_parse() returns tuple including netloc (eg example.com)
+        # if netloc is not empty, redirect to index
+        if not next_page or url_parse(next_page).netloc != '':
+            next_page = url_for('index')
+
+        return redirect(next_page)
+
     return render_template('login.html', title='Sign In', form=form)
 
 
