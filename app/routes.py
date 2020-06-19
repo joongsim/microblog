98f52a350f4f571e08d1fe97577cfcd3d1a42f2c
Joong Sim <joonjooly@gmail.com>
2020-06-18 18:31:37 -0700

    added last seen functionality

--- app/routes.py
+++ app/routes.py
@@ -1,9 +1,12 @@
-from flask import render_template, flash, redirect, url_for, request
-from flask_login import current_user, login_user, logout_user, login_required
+from datetime import datetime
+
+from flask import flash, redirect, render_template, request, url_for
+from flask_login import current_user, login_required, login_user, logout_user
+from werkzeug.urls import url_parse
+
 from app import app, db
 from app.forms import LoginForm, RegistrationForm
 from app.models import User
-from werkzeug.urls import url_parse
 
 
 @app.route('/')
@@ -85,3 +88,10 @@ def user(username):     # username comes from <username> in decorator
         {'author': user, 'body': 'Test post #2'}
     ]
     return render_template('user.html', user=user, posts=posts)
+
+
+@app.before_request
+def before_request():
+    if current_user.is_authenticated:
+        current_user.last_seen = datetime.utcnow()
+        db.session.commit()
