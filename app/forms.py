2c45b3285d97d3cc719dad723b763275e5a0f31f
Joong Sim <joonjooly@gmail.com>
2020-06-20 14:19:48 -0700

    Added username validation for editing profile.
    Previous versions did not warn user if they were trying to switch to a taken username and threw a 500 error instead.

--- app/forms.py
+++ app/forms.py
@@ -39,3 +39,15 @@ class EditProfileForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
     about_me = TextAreaField('About me', validators=[Length(min=0, max=140)])
     submit = SubmitField('Submit')
+
+    # Invokes FlaskForm parent class constructor
+    # Good practice to override method in both derived and parent classes
+    def __init__(self, original_username, *args, **kwargs):
+        super(EditProfileForm, self).__init__(*args, **kwargs)
+        self.original_username = original_username
+
+    def validate_username(self, username):
+        if username != self.original_username:
+            user = User.query.filter_by(username=self.username.data).first()
+            if user is not None:
+                raise ValidationError('Please use a different username.')
--- app/routes.py
+++ app/routes.py
@@ -93,13 +93,14 @@ def user(username):     # username comes from <username> in decorator
 @app.route('/edit_profile', methods=['GET', 'POST'])
 @login_required
 def edit_profile():
-    form = EditProfileForm()
+    form = EditProfileForm(current_user.username)
     if form.validate_on_submit():
         current_user.username = form.username.data
         current_user.about_me = form.about_me.data
         db.session.commit()
         flash('Your changes have been saved.')
         return redirect(url_for('edit_profile'))
+    # Pre-populate fields with current values for GET requests
     elif request.method == 'GET':
         form.username.data = current_user.username
         form.about_me.data = current_user.about_me
